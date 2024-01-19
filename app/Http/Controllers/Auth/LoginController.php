01e90510222924d8ecd3bd1099da9ebf2f35dc4d
Jonas Santos <jonashenrique965@gmail.com>
2024-01-19 00:57:38 -0300

    feat: login and logout

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    use HttpResponses;
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return $this->response('Authorized', Response::HTTP_OK, [
+                'token' => $request->user()->createToken('invoice')->plainTextToken
+            ]);
+        }
+
+        return $this->error('Unauthorized', Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->response('Token Revoked', Response::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -15,16 +17,18 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
 Route::get('/', function () {
     return response()->json([
         'success' => true
     ]);
 });
 
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
+Route::prefix('auth')->group(function () {
+    Route::post('login', [LoginController::class, 'login']);
+    Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
+    Route::post('register', [RegisterController::class, 'register']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class)->middleware('auth:sanctum');
+});
