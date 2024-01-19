8b57a9cf5f10b4ad62fbd4defa0fdebacca291f6
Jonas Santos <jonashenrique965@gmail.com>
2024-01-19 00:56:27 -0300

    feat: methods show, update and destroy added on UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
 use App\Models\User;
-use App\Http\Requests\UserStoreFormRequest;
+use App\Http\Requests\UserStoreUpdateFormRequest;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
+    use HttpResponses;
+
     public function __construct(
         protected User $repository,
     ) {
-
     }
 
     public function index()
@@ -21,7 +24,7 @@ class UserController extends Controller
         return UserResource::collection($users);
     }
 
-    public function store(UserStoreFormRequest $request)
+    public function store(UserStoreUpdateFormRequest $request)
     {
         $data = $request->validated();
         $data['password'] = bcrypt($data['password']);
@@ -29,5 +32,35 @@ class UserController extends Controller
         $user = $this->repository->create($data);
 
         return new UserResource($user);
+    }
+
+    public function show($id)
+    {
+        $user = $this->repository->findOrFail($id);
+
+        return new UserResource($user);
+    }
+
+    public function update(UserStoreUpdateFormRequest $request, string $id)
+    {
+        $user = $this->repository->findOrFail($id);
+
+        $data = $request->validated();
+
+        if ($request->password) {
+            $data['password'] = bcrypt($data['password']);
+        }
+
+        $user->update($data);
+
+        return new UserResource($user);
+    }
+
+    public function destroy($id)
+    {
+        $user = $this->repository->findOrFail($id);
+        $user->delete();
+
+        return $this->response('User deleted', Response::HTTP_OK);
     }
 }
