2b5f997df1ace129c88129ac7d5db69bc6cb5794
Jonas Santos <jonashenrique965@gmail.com>
2024-01-29 23:55:17 -0300

    Add admin user to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Factory as Faker;
-
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -31,5 +31,18 @@ class UserSeeder extends Seeder
         //         'profile_id' => $faker->numberBetween(1, 50),
         //     ]);
         // }
+
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'birth_date' => '1990-01-01',
+            'cpf' => 'null',
+            'address' => 'null',
+            'profile_type' => 'Professor',
+            'profile_id' => 1,
+        ]);
     }
 }
