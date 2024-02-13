2ddbc20238ba9afa362e82446b921b8fcdcb85cb
Jonas Santos <jonashenrique965@gmail.com>
2024-02-13 00:10:35 -0300

    Atualiza recurso MeResource para retornar campos adicionais para o perfil de Aluno

--- app/Http/Resources/MeResource.php
+++ app/Http/Resources/MeResource.php
@@ -15,17 +15,35 @@ class MeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'birth_date' => Carbon::parse($this->birth_date)->format('d/m/Y'),
-            'cpf' => $this->cpf,
-            'address' => $this->address,
-            'email' => $this->email,
-            'email_verified_at' => $this->email_verified_at,
-            'profile_type' => class_basename($this->profile_type),
-            'created_at' => Carbon::parse($this->created_at)->format('d/m/Y H:i:s'),
-            'updated_at' => Carbon::parse($this->updated_at)->format('d/m/Y H:i:s'),
-        ];
+        if ($this->profile_type === 'App\Models\Aluno') {
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'birth_date' => Carbon::parse($this->birth_date)->format('d/m/Y'),
+                'cpf' => $this->cpf,
+                'address' => $this->address,
+                'email' => $this->email,
+                'email_verified_at' => Carbon::parse($this->email_verified_at)->format('d/m/Y H:i:s'),
+                'plano' => $this->plano,
+                'vencimento' => Carbon::parse($this->vencimento)->format('d/m/Y'),
+                'status' => $this->status,
+                'profile_type' => class_basename($this->profile_type),
+                'created_at' => Carbon::parse($this->created_at)->format('d/m/Y H:i:s'),
+                'updated_at' => Carbon::parse($this->updated_at)->format('d/m/Y H:i:s'),
+            ];
+        } else {
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'birth_date' => Carbon::parse($this->birth_date)->format('d/m/Y'),
+                'cpf' => $this->cpf,
+                'address' => $this->address,
+                'email' => $this->email,
+                'email_verified_at' => $this->email_verified_at,
+                'profile_type' => class_basename($this->profile_type),
+                'created_at' => Carbon::parse($this->created_at)->format('d/m/Y H:i:s'),
+                'updated_at' => Carbon::parse($this->updated_at)->format('d/m/Y H:i:s'),
+            ];
+        }
     }
 }
