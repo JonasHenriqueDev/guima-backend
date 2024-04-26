fae5ab9cc4acf47c082f113753bc016d54e41d2b
Jonas Santos <jonashenrique965@gmail.com>
2024-04-26 23:35:37 +0000

    add on delete cascade

---
+++ database/migrations/2024_04_26_203254_add_on_delete_cascade_on_aulas.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('aulas', function (Blueprint $table) {
+            $table->foreign('submodulo_id')
+                ->references('id')
+                ->on('submodulos')
+                ->onDelete('cascade'); // Adiciona a opção ON DELETE CASCADE
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('aulas', function (Blueprint $table) {
+            $table->dropForeign(['submodulo_id']); // Remove a chave estrangeira
+        });
+    }
+};
